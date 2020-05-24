0d3728df80a7f182600cb18ad04ad43e330cda25
Gail-mm <63254514+Gail-mm@users.noreply.github.com>
2020-05-24 10:31:05 +0100

    build

---
+++ precache-manifest.e2423c286a0f37db3ba29b26dca563ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0242ce42ed19162b90ca7652bb954081",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29ba27d36a82efc5b3fe",
+    "url": "/static/css/main.81f7b645.chunk.css"
+  },
+  {
+    "revision": "f585964ed89d1f61f83a",
+    "url": "/static/js/2.e0af3411.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e0af3411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29ba27d36a82efc5b3fe",
+    "url": "/static/js/main.4366f2c3.chunk.js"
+  },
+  {
+    "revision": "e191e0d367c27e5b9a7e",
+    "url": "/static/js/runtime-main.9c01660e.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
